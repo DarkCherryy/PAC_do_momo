689fc387f2ff0dbd31a097b62cd234afc08ac1cb
DarkCherryy <rafaelalorencettinasci@gmail.com>
2026-05-29 21:50:13 -0300

    Mapas ato II e III

---
+++ Mapas/45296b82-85e1-461a-894d-f199236e03f1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="45296b82-85e1-461a-894d-f199236e03f1" tilewidth="32" tileheight="32" tilecount="704" columns="32">
+ <image source="../Assets/45296b82-85e1-461a-894d-f199236e03f1.jpeg" width="1024" height="711"/>
+</tileset>
---
+++ Mapas/download (1).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="download (1)" tilewidth="32" tileheight="32" tilecount="204" columns="17">
+ <image source="../Assets/download (1).png" width="573" height="397"/>
+</tileset>
